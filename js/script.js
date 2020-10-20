193ca0cfb3a7b917435b8479c85fb295d019f57f
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-10-20 03:39:34 -0400

    Created skeleton for about me section, added links and styles.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Shrink the header when use scrolls down
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
@@ -9,4 +8,9 @@ function scrollFunction() {
     document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("name").style.fontSize = "4.25vw";
   }
+}
+
+function scrollToAbout() {
+  elmt = document.getElementById("aboutsection");
+  elmt.scrollIntoView();
 }
